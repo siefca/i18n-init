f8ad3f2c91968187ea623a5c1daa7bdff411b1b6
Paweł Wilk <siefca@gnu.org>
2012-11-13 15:38:17 +0100

    Added some patches for backward compatibility

--- lib/i18n-init/patches.rb
+++ lib/i18n-init/patches.rb
@@ -4,18 +4,41 @@
 #
 # This file contains patches that add blank?, present? and presence methods to Object class if not defined before.
 
-# @abstract
+# @private
+# @abstract This class is here for compatibility reasons.
 class Object
+  unless method_defined?(:public_send)
+    # @private
+    def public_send(name, *args)
+      unless public_methods.include?(name.to_s)
+        raise NoMethodError.new("undefined method `#{name}' for \"#{self.inspect}\":#{self.class}")
+      end
+      send(name, *args)
+    end
+  end
+
+  unless method_defined?(:public_method)
+    # @private
+    def public_method(name)
+      unless public_methods.include?(name.to_s)
+        raise NameError.new("undefined method `#{name}' for class `#{self.class}'")
+      end
+      method(name)
+    end
+  end
+
   unless method_defined?(:blank?)
     # @private
     def blank?; respond_to?(:empty?) ? empty? : !self end
   end
+
   unless method_defined?(:present?)
     # @private
     def present?; !blank? end
   end
+
   unless method_defined?(:presence)
     # @private
     def presence; self if present? end
   end
-end
+end # class Object
