b68f3c2061c705829df6cc378763afb88507f5d4
Paweł Wilk <siefca@gnu.org>
2012-11-24 23:35:36 +0100

    Environment YAML sections support

--- lib/i18n-init/settings.rb
+++ lib/i18n-init/settings.rb
@@ -11,11 +11,22 @@ class I18n::Init
   module Settings
 
     # Loads settings if needed and returns settings hash.
+    # Uses environment section if environment is set and a corresponding section
+    # is present in YAML file.
+    # 
+    # @return [Hash] settings
     def settings
-      return @settings unless @settings.blank?
-      @settings = yaml_load(config_file)
+      @settings ||= yaml_load(config_file).tap do |s|
+        if s.is_a?(Hash) && environment.present? && s.has_key?(environment)
+          p_debug "switching to environment section: #{environment}"
+          break s[environment]
+        end
+      end || {}
     end
 
+    # Loads bundled settings (if needed) and returns settings hash.
+    # 
+    # @return [Hash] settings
     def settings_bundled
       return @settings_bundled unless @settings_bundled.blank?
       @settings_bundled = yaml_load(bundled_settings_file)
@@ -26,12 +37,15 @@ class I18n::Init
     # Loads settings from YAML file.
     def yaml_load(fname)
       return {} unless File.exists?(fname)
+      p_debug "reading settings from file: #{fname}"
       File.open(fname).tap { |f| return YAML::load(f).tap{ f.close } || {} }
     end
 
     # Invalidates cached settings based on configuration file contents.
     def invalidate_caches
+      p_debug "invalidating caches"
       @settings = nil
+      @settings_bundled = nil
       super if defined?(super)
     end
 
