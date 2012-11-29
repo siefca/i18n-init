82d51904668bb43ae5c6e210ff34bf2a98cc2fe6
Paweł Wilk <siefca@gnu.org>
2012-11-29 22:32:30 +0100

    ConfBlock module documented and improved a bit

--- lib/i18n-init/confblock.rb
+++ lib/i18n-init/confblock.rb
@@ -2,7 +2,7 @@
 # Copyright:: (c) 2012 by Paweł Wilk
 # License::   This program is licensed under the terms of {file:LGPL-LICENSE GNU Lesser General Public License} or {file:COPYING Ruby License}.
 #
-# This file contains I18n::Init::Block module.
+# This file contains I18n::Init::Block module used to dynamically create anonymous proxy modules.
 
 require 'singleton'
 
@@ -10,18 +10,32 @@ require 'singleton'
 class I18n::Init
 
   # This module installs configuration block processing module
-  # and its proxy method.
+  # and its proxy methods.
   module ConfBlock
 
+    # @private
     def self.included(base)
       base.extend(ClassMethods)
     end
 
-    def conf_block
+    # Returns configuration module.
+    # 
+    # @return [Module] anonymous module with proxy module methods
+    def configuration_block
       self.class.conf_module
     end
 
+    def configuration_block_delegated
+      self.class.configuration_block_delegated
+    end
+
+    # This module contains methods that will extend the class.
     module ClassMethods
+
+      # This method creates and returns anonymous module containing
+      # delegators that point to methods from a class this module is included in.
+      # 
+      # @return [Module] anonymous module with proxy module methods
       def conf_module
         @conf_module ||= Module.new.tap do |cm|
           instance.p_debug "creating anonymous proxy module for evaluating configuration block"
@@ -40,10 +54,24 @@ class I18n::Init
         end
       end
 
+      # This is DSL method intended to be used in a class to indicate which methods
+      # should be delegated from anonymous proxy module available when {conf_module} class method
+      # is called.
+      # 
+      # @param methods [Array<Symbol>] list of method names
+      # @return [nil]
       def configuration_block_delegate(*methods)
         @cf_block_delegators ||= []
         @cf_block_delegators.concat(methods)
+        nil
       end
+      alias_method :configuration_method, :configuration_block_delegate
+      alias_method :settings_method,      :configuration_block_delegate
+
+      def configuration_block_delegated
+        @cf_block_delegators.dup
+      end
+
     end # module ClassMethods
 
   end # module ConfBlock
